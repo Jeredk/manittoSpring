2758b1fc67fc9c8513c395dcbbf36d2c3c5e9d76
Jeredk <qpfmsktm9@gmail.com>
2019-10-27 16:31:48 +0900

    52

--- Manitto/src/main/java/com/ymd/manitto/controller/LogInController.java
+++ Manitto/src/main/java/com/ymd/manitto/controller/LogInController.java
@@ -1,5 +1,8 @@
 package com.ymd.manitto.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -16,7 +19,7 @@ public class LogInController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	@RequestMapping(value = "/logIn", method = RequestMethod.GET)
-	public String home() {
+	public String home(HttpServletRequest req,HttpSession httpSession) {
 
 		return "logIn_View";
 	}
