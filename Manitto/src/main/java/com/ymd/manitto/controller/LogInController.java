66e0c576b02233ad20fabf9f51c053cf8b13085a
Jeredk <qpfmsktm9@gmail.com>
2019-10-27 17:30:00 +0900

    ㄴ

--- Manitto/src/main/java/com/ymd/manitto/controller/LogInController.java
+++ Manitto/src/main/java/com/ymd/manitto/controller/LogInController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ymd.manitto.HomeController;
 
@@ -19,8 +21,20 @@ public class LogInController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	@RequestMapping(value = "/logIn", method = RequestMethod.GET)
-	public String home(HttpServletRequest req,HttpSession httpSession) {
+	public ModelAndView firstPage(HttpServletRequest req,HttpSession httpSession,ModelAndView mav) {
+		
+		mav.setViewName("logIn_View");
 
-		return "logIn_View";
+		return mav;
+	}
+	
+	@RequestMapping(value = "/signUp", method = RequestMethod.GET)
+	public ModelAndView signUpPage(HttpServletRequest req,HttpSession httpSession,ModelAndView mav) {
+		
+		mav.setViewName("signUp_View");
+		
+		return mav;
 	}
 }
+
+
---
+++ Manitto/src/main/webapp/WEB-INF/views/signUp_View.jsp
@@ -0,0 +1,22 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false"%>
+<html>
+<head>
+<title>manitto sign_UP</title>
+</head>
+<body>
+	<h1>회원가입</h1>
+
+	<P>힌트를 알려주세요.</P>
+
+	<form>
+
+		색깔<input type="text" name="favoriteColor" /> </br>
+		음식<input type="text" name="favoriteFood" /> </br>
+		가수<input type="text" name="favoriteSinger" /></br>
+		영화장르<input type="text" name="favoriteMovieGenre" /> </br>
+		인스타그램 <input type="text" name="instagramAccount" /> </br>
+		<input type="submit" value="완료" />
+	</form>
+</body>
+</html>
