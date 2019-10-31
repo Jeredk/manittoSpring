f3322f230836512b7aaf0e56ec514b454b065e1d
cha7713 <cha7713@gmail.com>
2019-10-31 18:33:06 +0900

    dd

--- Manitto/src/main/java/com/ymd/manitto/controller/likeController.java
+++ Manitto/src/main/java/com/ymd/manitto/controller/likeController.java
@@ -1,27 +1,18 @@
 package com.ymd.manitto.controller;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.ymd.manitto.Gps;
-import com.ymd.manitto.HomeController;
-import com.ymd.manitto.User;
-import com.ymd.manitto.service.GpsService;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 import com.ymd.manitto.service.likeService;
 import com.ymd.manitto.utils.StringUtils;
 
@@ -37,12 +28,18 @@ public class likeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(likeController.class);
 	
-	@RequestMapping(value = "/likeYou", method = RequestMethod.POST)
+	@RequestMapping(value = "/likeyou", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> likeYou(@RequestBody Map<String, Object> map){
 		logger.debug(String.valueOf(map.get("TARGET")));
 		logger.debug(String.valueOf(map.get("STALKER")));
-		int result = likeser.likeyou(map);
+		int result = 0;
+		try {
+			result = likeser.likeyou(map);			
+		}catch (MySQLIntegrityConstraintViolationException e) {
+			e.printStackTrace();
+			result = 2;
+		}
 		map.put("result", result);
 		
 		return map;
--- Manitto/src/main/java/com/ymd/manitto/service/likeService.java
+++ Manitto/src/main/java/com/ymd/manitto/service/likeService.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 import com.ymd.manitto.dao.likeDAO;
 
 @Service
@@ -13,8 +14,10 @@ public class likeService {
 	@Autowired
 	likeDAO likeD;
 
-	public int likeyou(Map<String, Object> map) {
-		return likeD.likeyou(map);
+	public int likeyou(Map<String, Object> map) throws MySQLIntegrityConstraintViolationException {
+		int result = 0;
+		result = likeD.likeyou(map);
+		return result;
 
 	}
 
